5debd293f7fe9b3eda97fd067eda3e915fc923fa
winter4 <mywinter4@gmail.com>
2024-10-26 18:26:04 +0300

    fix: tests

--- tests/api/image.test.js
+++ tests/api/image.test.js
@@ -4,31 +4,12 @@ const path = require('path');
 const {api, db} = require('../setup');
 const should = require('should');
 
-/* - - - - - - - - - - - - - - - - - - */
-
-const user = {
-	email: 'user@example.com',
-	password: 'qwerty123'
-};
-
-let loginCookies = null;
-
-/* - - - - - - - - - - - - - - - - - - */
-
 const clean = async() => {
 	await db('images').delete({});
-	await db('users').delete({});
 };
 
-describe.only('API / Image', () => {
+describe('API / Image', () => {
 	before(clean);
-	before('create & login user', async() => {
-		await api.post('/auth/sign-up').send(user).expect(200);
-		const loginResp = await api.post('/auth/sign-in').send(user).expect(200);
-		loginCookies = loginResp.headers['set-cookie'];
-		should(loginCookies).match(new RegExp('.*sessionid=.*'));
-	});
-
 	after(clean);
 
 	describe('#upload', () => {
@@ -36,7 +17,6 @@ describe.only('API / Image', () => {
 			const response = await api
 				.post('/image/upload')
 				.expect(200)
-				.set('Cookie', [loginCookies])
 				.attach('image', path.join(__dirname, '..', 'assets', 'mountains.jpg'));
 
 			const {body: {data: result}} = response;
@@ -48,8 +28,7 @@ describe.only('API / Image', () => {
 		it('should work', async() => {
 			const response = await api
 				.post('/image/get')
-				.expect(200)
-				.set('Cookie', [loginCookies]);
+				.expect(200);
 
 			const {body: {data: {values, count}}} = response;
 			should(count).aboveOrEqual(1);
@@ -64,15 +43,38 @@ describe.only('API / Image', () => {
 		it('should work', async() => {
 			const {body: {data: {values}}} = await api
 				.post('/image/get')
-				.expect(200)
-				.set('Cookie', [loginCookies]);
+				.expect(200);
 
 			const [value] = values;
 
 			const response = await api
 				.get(`/image/download/${value.id}`)
-				.expect(200)
-				.set('Cookie', [loginCookies]);
+				.expect(200);
+			should(response).match({
+				statusCode: 200,
+				headers: {
+					'content-type': 'image/jpeg'
+				}
+			});
+		});
+	});
+
+	describe('#process', () => {
+		let imageId = null;
+
+		before('get image', async() => {
+			const {body: {data: {values}}} = await api
+				.post('/image/get')
+				.expect(200);
+
+			imageId = values[0].id;
+		});
+
+		it('should work with filter: gaussian-blur', async() => {
+			const response = await api
+				.post('/image/process')
+				.send({imageId, filter: 'gaussian-blur'})
+				.expect(200);
 			should(response).match({
 				statusCode: 200,
 				headers: {
