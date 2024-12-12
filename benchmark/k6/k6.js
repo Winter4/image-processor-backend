2f610ca2fad0143b9efcb78beade1679fd789169
winter4 <mywinter4@gmail.com>
2024-12-12 12:19:25 +0300

    tmp

--- benchmark/k6/k6.js
+++ benchmark/k6/k6.js
@@ -1,37 +1,34 @@
 import http from 'k6/http';
 import {check} from 'k6';
-// import {readFileSync} from 'k6/fs';
 
 export const options = {
-	vus: 10,         // Количество виртуальных пользователей
-	duration: '1m',  // Время нагрузки (1 минута)
+	vus: 10,         // Количество виртуальных пользователей (aka RPS)
+	duration: '10s',  // Время нагрузки
 };
 
-/* export default function () {
-	// Читаем файл из смонтированной директории
-	const fileData = readFileSync('/k6/images/mountains.jpg', 'b'); // 'b' - для бинарного чтения
+// Читаем файл из смонтированной директории
+// eslint-disable-next-line no-undef
+const fileData = open('/k6/images/mountains.jpg', 'b'); // 'b' - для бинарного чтения
 
-	// Создаём payload для multipart-запроса
-	const payload = {
-		image: http.file(fileData, 'mountains.jpg')
-	};
-	const headers = {
-		'Content-Type': 'multipart/form-data',
-	};
+// Создаём payload для multipart-запроса
+const payload = {
+	image: http.file(fileData, 'mountains.jpg')
+};
+const url = 'http://app:5001/image/upload';
 
-	const url = 'http://app:5001/image/upload';
-	const res = http.post(url, payload, {headers});
+export default function () {
+	const res = http.post(url, payload);
 
 	check(res, {
 		'status is 200': r => r.status === 200,
 	});
-} */
+}
 
-export default function() {
+/* export default function() {
 	const url = 'http://app:5001/ping/';
 	const res = http.get(url);
 
 	check(res, {
 		'status is 200': r => r.status === 200
 	});
-}
+} */
