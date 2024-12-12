3e5c0e9097bdabba6e92cd2b46b967020bb25e7e
winter4 <mywinter4@gmail.com>
2024-12-12 15:14:41 +0300

    feat: k6 native-processing

---
+++ benchmark/k6/k6.js
@@ -0,0 +1,37 @@
+import http from 'k6/http';
+import {check} from 'k6';
+
+const HOST = 'http://app:5001';
+
+export const options = {
+	stages: [
+		{duration: '1m', target: 10},
+		{duration: '4m', target: 50}
+	]
+};
+
+// Читаем файл из смонтированной директории
+// eslint-disable-next-line no-undef
+const fileData = open('/k6/images/mountains.jpg', 'b'); // 'b' - для бинарного чтения
+
+export function setup() {
+	const body = http
+		.post(`${HOST}/image/upload`, {image: http.file(fileData, 'mountains.jpg')})
+		.json();
+
+	return body.data;
+}
+
+export default function({id}) {
+	const url = `${HOST}/image/process-native`;
+
+	const res = http.post(
+		url,
+		JSON.stringify({imageId: id, filter: 'gaussian-blur'}),
+		{headers: {'Content-Type': 'application/json'}}
+	);
+
+	check(res, {
+		'status is 200': r => r.status === 200,
+	});
+}
