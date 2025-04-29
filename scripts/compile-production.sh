8e5a0da192234a266d0ce35fcee3911a8f16855c
winter4 <mywinter4@gmail.com>
2025-04-30 00:10:02 +0300

    feat: completing workflow

--- scripts/compile-production.sh
+++ scripts/compile-production.sh
@@ -2,4 +2,7 @@ npx tsc
 npx tsc-alias
 
 cp ./source/api/image/image.wasm-processor/multithread/*.wasm ./compiled/source/api/image/image.wasm-processor/multithread
-cp ./source/api/image/image.wasm-processor/singlethread/*.js ./compiled/source/api/image/image.wasm-processor/singlethread
+cp ./source/api/image/image.wasm-processor/multithread/*.js   ./compiled/source/api/image/image.wasm-processor/multithread
+
+cp ./source/api/image/image.wasm-processor/singlethread/*.wasm ./compiled/source/api/image/image.wasm-processor/singlethread
+cp ./source/api/image/image.wasm-processor/singlethread/*.js   ./compiled/source/api/image/image.wasm-processor/singlethread
--- tests/setup.js
+++ tests/setup.js
@@ -3,7 +3,11 @@ const fs = require('fs');
 const supertest = require('supertest');
 const path = require('path');
 
-const config = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../config', 'config.json')));
+const configPath = process.env.NODE_ENV === 'production'
+	? path.resolve(__dirname, '../compiled/config', 'config.json')
+	: path.resolve(__dirname, '../config', 'config.json');
+
+const config = JSON.parse(fs.readFileSync(configPath));
 const api = supertest(`http://localhost:${config.deploy.apiPort}`);
 
 module.exports = {config, api};
